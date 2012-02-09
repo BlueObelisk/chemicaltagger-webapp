e8c670feb02fe4861dcfee816c00c69a66c171c3
Daniel Lowe <dl387@cam.ac.uk>
2012-02-09 13:35:32 +0000

    Used default constructors for ChemicalTagger and ACPgeo hence fixed ACP specific POS tagging not being done properly

--- src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/webdemo/SubmitResource.java
+++ src/main/java/uk/ac/cam/ch/wwmm/chemicaltagger/webdemo/SubmitResource.java
@@ -1,15 +1,11 @@
 package uk.ac.cam.ch.wwmm.chemicaltagger.webdemo;
 
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.Arrays;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import nu.xom.Document;
 
-import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringEscapeUtils;
 import org.jsoup.Jsoup;
 import org.restlet.data.Form;
@@ -18,18 +14,12 @@ import org.restlet.representation.Representation;
 import org.restlet.resource.Post;
 import org.restlet.resource.ServerResource;
 
-import uk.ac.cam.ch.wwmm.acpgeo.ACPRegexTagger;
 import uk.ac.cam.ch.wwmm.acpgeo.ACPSentenceParser;
+import uk.ac.cam.ch.wwmm.acpgeo.ACPTagger;
 import uk.ac.cam.ch.wwmm.chemicaltagger.ChemistryPOSTagger;
 import uk.ac.cam.ch.wwmm.chemicaltagger.ChemistrySentenceParser;
-import uk.ac.cam.ch.wwmm.chemicaltagger.OpenNLPTagger;
-import uk.ac.cam.ch.wwmm.chemicaltagger.OscarTagger;
-import uk.ac.cam.ch.wwmm.chemicaltagger.OscarTokeniser;
 import uk.ac.cam.ch.wwmm.chemicaltagger.POSContainer;
-import uk.ac.cam.ch.wwmm.chemicaltagger.RegexTagger;
 import uk.ac.cam.ch.wwmm.chemicaltagger.SentenceParser;
-import uk.ac.cam.ch.wwmm.chemicaltagger.Tagger;
-import uk.ac.cam.ch.wwmm.chemicaltagger.WhiteSpaceTokeniser;
 import uk.ac.cam.ch.wwmm.oscar.Oscar;
 
 /**
@@ -47,36 +37,30 @@ public class SubmitResource extends ServerResource {
 	}
     @Post("form:txt")
     public Representation doForm(Form form) throws IOException {
-    	ChemistryPOSTagger chemPos;
-    	SentenceParser parser ;
-    	Document doc;
         String body = form.getFirstValue("body");
         String chemistryType = form.getFirstValue("ChemistryType");
         WebdemoApplication webDemo = (WebdemoApplication) getApplication();
-        OscarTagger oscarTagger = new OscarTagger(getOscarInstance());
-        OpenNLPTagger openNLPTagger = OpenNLPTagger.getInstance();
-        
+
+        POSContainer container;
         if (chemistryType.equalsIgnoreCase("Atmospheric")){
-        	List<Tagger> taggers = Arrays.asList(new ACPRegexTagger(), oscarTagger, openNLPTagger);
-        	chemPos = new ChemistryPOSTagger(new WhiteSpaceTokeniser(), taggers);
+        	container = ACPTagger.getInstance().runTaggers(body);
         }
         else {
-        	List<Tagger> taggers = Arrays.asList(new RegexTagger(), oscarTagger, openNLPTagger);
-        	chemPos = new ChemistryPOSTagger(new OscarTokeniser(), taggers);
+        	container = ChemistryPOSTagger.getDefaultInstance().runTaggers(body);
         }
-        POSContainer container = chemPos.runTaggers(body);
-        InputStream taggedStream = IOUtils.toInputStream(container.getTokenTagTupleAsString(), "UTF-8");
+
+       	SentenceParser parser;
         if (chemistryType.equalsIgnoreCase("Atmospheric")){
-        	parser = new ACPSentenceParser(taggedStream);
+        	parser = new ACPSentenceParser(container.getTokenTagTupleAsString());
         	
         }        
         else {
-        	parser = new ChemistrySentenceParser(taggedStream);
+        	parser = new ChemistrySentenceParser(container.getTokenTagTupleAsString());
 
         }
         parser.parseTags();
         
-        doc = parser.makeXMLDocument();
+        Document doc = parser.makeXMLDocument();
         Map<String,Object> model = new HashMap<String, Object>();
         XMLtoHTML xmltoHTML = new XMLtoHTML();
         xmltoHTML.convert(doc);		
